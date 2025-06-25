3ba472dc46dc2b280daf426ab42051a0dd44cb31
23249 <23249@students.mrgs.school.nz>
2025-06-26 09:06:23 +1200

    Making Changes

--- java.js
+++ java.js
@@ -35,7 +35,7 @@ function showSlides() {
   let slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-  }
+  }Z
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
